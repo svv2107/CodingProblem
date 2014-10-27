94092dbc7d67ba8b8fcb8bb5169e1acc93baaaaf
Steven Vuong <svv2107@columbia.edu>
2014-10-27 01:20:54 -0400

    Changed function name

--- src/RunProblem1.java
+++ src/RunProblem1.java
@@ -14,16 +14,16 @@ public class RunProblem1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] anArray = {1,2};
-		System.out.println(Arrays.toString(consecutive3(anArray)));
+		System.out.println(Arrays.toString(findConsecutiveRuns(anArray)));
 		
 		int[] anArray2 = {1,2,3,5,10,9,8,9,10,11,7};
-		System.out.println(Arrays.toString(consecutive3(anArray2)));
+		System.out.println(Arrays.toString(findConsecutiveRuns(anArray2)));
 		
 		int[] anArray3 = {};
-		System.out.println(Arrays.toString(consecutive3(anArray3)));
+		System.out.println(Arrays.toString(findConsecutiveRuns(anArray3)));
 	}
 	
-	public static int[] consecutive3(int[] inArray){
+	public static int[] findConsecutiveRuns(int[] inArray){
 		int consecutiveCount=0;
 		int[] tempArray = new int[inArray.length]; 									//We don't know the length of the array yet
 		for(int i=0;i<inArray.length-2;i++){
